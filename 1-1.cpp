c8917544c6e22f5836f66f1a04f5920283344d18
u-na-gi <orcinusorca1758dv6932@gmail.com>
2024-07-14 16:08:43 +0900

    atcoderをcppで事始め

---
+++ 1-1.cpp
@@ -0,0 +1,45 @@
+
+// 問題文
+
+// 高橋君はデータの加工が行いたいです。
+
+// 整数
+// a,b,cと、文字列 s が与えられます。 a+b+c の計算結果と、文字列 s を並べて表示しなさい。 
+
+// 入力
+
+// 入力は以下の形式で与えられる。
+
+// a
+
+// b 
+
+// c
+
+// s
+
+// 出力
+
+// a+b+c と s を空白区切りで 1 行に出力せよ。 
+// https://atcoder.jp/contests/practice/tasks/practice_1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 整数の入力
+    int a;
+    cin >> a;
+    // スペース区切りの整数の入力
+    int b, c;
+    cin >> b >> c;
+
+    // 文字列の入力
+    string s;
+    cin >> s;
+    // 出力
+    cout << (a+b+c) << " " << s << endl;
+    
+    return 0;
+}
+
